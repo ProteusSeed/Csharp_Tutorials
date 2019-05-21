de8ae2abb5ec9633bbb67dc84fafcdd610e2de53
unknown <stanlin.caceres@gmail.com>
2019-05-21 09:15:29 -0400

    Basic 1 done

---
+++ Basic_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Basic_1
+{
+    /*
+     1. Write a C# Sharp program to print Hello and your name in a separate line. 
+        Expected Output : 
+        Hello: Alexandra Abramov
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello \nStan Caceres");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,2 @@
+
+This solution contains C# Practice exercises from: https://www.w3resource.com/csharp-exercises
