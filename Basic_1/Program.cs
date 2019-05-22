9baee6b0be7179504cefde27e81e7e77a541f684
unknown <stanlin.caceres@gmail.com>
2019-05-22 17:41:07 -0400

    Set up main program to run exercises.

--- Basic_1/Program.cs
+++ Basic_1/Program.cs
@@ -11,8 +11,56 @@ namespace Basic_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello \nStan Caceres");
-            Console.ReadLine();
+            Boolean run = true;
+            int exercise;
+            string entry;
+
+            while (run == true)
+            {
+                Console.WriteLine("Which Excercise shall we run?: ");
+                entry = Console.ReadLine();
+
+                if (entry == "x") { break; };
+
+                int.TryParse(entry, out exercise);
+
+                //do some validation here in case not an int
+
+                switch (exercise)
+                {
+                    case 1:
+                        Basic_1();
+                        break;
+
+                    default:
+                        Console.WriteLine("No such Excercise exists.");
+
+                        break;
+                }
+
+
+                while (entry != "")
+                {
+                    Console.WriteLine("\nPress [ENTER] to continue, \"x\" to end");
+                    entry = Console.ReadLine();
+
+                    if (entry == "x")
+                    {
+                        run = false;
+                        break;
+                    }
+                }
+
+                exercise = 0;
+            }
+            
         }
+
+        static void Basic_1()
+        {
+            Console.WriteLine("Hello \nStan Caceres");
+
+        }
+
     }
 }
