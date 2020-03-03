d2f2d962b744d6fdd43fb931e18fe18dbb96bc18
unknown <stanlin.caceres@gmail.com>
2020-03-03 11:21:53 -0500

    Conditional Statements, Exercise 1 compoleted.

---
+++ ConditionalStatement/ConditionalStatementExcercises.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Exercises;
+
+namespace Conditional_Statement
+{
+    public static class Exercise_1
+    {
+        /*
+            1. Write a C# Sharp program to accept two integers and check whether they are equal or not. 
+            Test Data :
+            Input 1st number: 5 
+            Input 2nd number: 5 
+            Expected Output: 5 and 5 are equal 
+        */
+
+        public static void Execute()
+        {
+            int firstNumber = UtilityMethods.GetValidNumber();
+            int secondNumber = UtilityMethods.GetValidNumber();
+
+            string Condition = firstNumber == secondNumber ? " " : " not ";
+
+            Console.WriteLine($"{firstNumber} and {secondNumber} are{Condition}equal.");
+        }
+    }
+}
---
+++ ConditionalStatement/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Exercises;
+
+namespace Conditional_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continueLoop = true;
+            int number = 0;
+
+            while (continueLoop == true)
+            {
+                continueLoop = UtilityMethods.getValidProgramNumber(out number);
+
+                switch (number)
+                {
+                    case 1:
+                        Exercise_1.Execute();
+                        break;
+
+                    case 0:
+                        //exit
+                        continueLoop = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Program does not exist!");
+                        break;
+                }
+
+            }
+        }
+    }
+}
