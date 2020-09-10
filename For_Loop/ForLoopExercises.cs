e2c06fce65664a84d0a239bd6bdd1c2d39bde12c
unknown <stanlin.caceres@gmail.com>
2020-09-10 18:35:06 -0400

    For Loop exercise 1 through 3

---
+++ For_Loop/ForLoopExercises.cs
@@ -0,0 +1,73 @@
+﻿using Exercises;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace For_Loop
+{
+    public static class Exercise_1
+    {
+        /*
+         1. Write a program in C# Sharp to display the first 10 natural numbers.
+            Expected Output: 1 2 3 4 5 6 7 8 9 10
+         */
+
+        public static void Execute()
+        {
+            int length = 10;
+
+            for (int i = 1; i <= length; i++)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+
+    public static class Exercise_2
+    {
+        /*2. Write a C# Sharp program to find the sum of first 10 natural numbers. 
+            Expected Output :
+            The first 10 natural number is :
+            1 2 3 4 5 6 7 8 9 10
+            The Sum is : 55
+        */
+
+        public static void Execute()
+        {
+            int length = 10;
+            int result = 0;
+
+            for (int i = 1; i <= length; i++)
+            {
+                result += i;
+            }
+
+            Console.WriteLine("The sum is: {0}", result);
+        }
+    }
+
+    public static class Exercise_3
+    {
+        /*
+          3. Write a program in C# Sharp to display n terms of natural number and their sum.
+            Test Data : 7
+            Expected Output :
+            The first 7 natural number is :
+            1 2 3 4 5 6 7
+            The Sum of Natural Number upto 7 terms : 28
+         */
+        public static void Execute()
+        {
+            int numberOfNumbers = UtilityMethods.GetValidAbsoluteNumber("Enter number of natural numbers to sum: ");
+            int result = 0;
+
+            for (int i = 0; i <= numberOfNumbers; i++)
+            {
+                result += i;
+            }
+
+            Console.WriteLine("The Sum of Natural Number upto 7 terms : {0}", result);
+        }
+    }
+}
---
+++ For_Loop/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Exercises;
+
+namespace For_Loop
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                bool continueLoop = true;
+                int number = 0;
+
+                while (continueLoop == true)
+                {
+                    continueLoop = Exercises.UtilityMethods.getValidProgramNumber(out number);
+
+                    switch (number)
+                    {
+                        case 1:
+                            Exercise_1.Execute();
+                            break;
+
+                        case 2:
+                            Exercise_2.Execute();
+                            break;
+
+                        case 3:
+                            Exercise_3.Execute();
+                            break;
+                    default:
+                            Console.WriteLine("Program does not exist!");
+                            break;
+                    }
+
+                }
+            }
+
+    }
+}
