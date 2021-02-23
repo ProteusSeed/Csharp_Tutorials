f0697081c4e2d0d01896365f59973a883cc9cbed
unknown <stanlin.caceres@gmail.com>
2021-02-23 17:52:51 -0500

    Arrays: Exercise 2 completed.

---
+++ Arrays/Array_Exercises.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Exercises;
+
+namespace Arrays
+{
+    public static class Exercise_1
+    {
+        /*
+         * 1. Write a program in C# Sharp to store elements in an array and print it. 
+            Test Data:
+            Input 10 elements in the array:
+            element - 0 : 1
+            element - 1 : 1
+            element - 2 : 2
+            .......
+            Expected Output :
+            Elements in array are: 1 1 2 3 4 5 6 7 8 9
+        */
+        public static void Execute()
+        {
+            string[] elements = new string[10];
+
+            Console.WriteLine("Input 10 elements in the array");
+
+            for( int element = 0; element < 10; element++)
+            {
+                elements[element] = UtilityMethods.GetValidString($"Element {element + 1}");
+            }
+
+            foreach (var item in elements)
+            {
+                Console.Write($"{item} ");
+            }
+
+            return;
+        }
+    }
+
+    public static class Exercise_2
+    {
+        /*
+         * 2. Write a program in C# Sharp to read n number of values in an array and display it in reverse order.
+        Test Data :
+        Input the number of elements to store in the array :3
+        Input 3 number of elements in the array :
+        element - 0 : 2
+        element - 1 : 5
+        element - 2 : 7
+        Expected Output:
+        The values store into the array are:
+        2 5 7
+        The values store into the array in reverse are :
+        7 5 2
+         */
+
+        public static void Execute()
+        {
+            int numOfElements = UtilityMethods.GetValidAbsoluteNumber("Input the number of elements to store in the array");
+            string[] elements = new string[numOfElements];
+
+            Console.WriteLine("Input {0} elements into array:", numOfElements);
+
+            for( int e = 0; e < numOfElements; e++ )
+            {
+                Console.Write($"Element - {e}: ");
+                elements[e] = Console.ReadLine();
+            }
+
+            Console.WriteLine();
+            Console.Write($"The values stored in the array are: ");
+
+            foreach( var item in elements)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine();
+            Console.Write($"The values stored in reverse are: ");
+
+            Array.Reverse(elements);
+
+            foreach (var item in elements)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continueLoop = true;
+            int number = 0;
+
+            while (continueLoop == true)
+            {
+                continueLoop = Exercises.UtilityMethods.getValidProgramNumber(out number);
+
+                switch (number)
+                {
+                    case 1:
+                        Exercise_1.Execute();
+                        break;
+
+                    case 2:
+                        Exercise_2.Execute();
+                        break;
+                    default:
+                        Console.WriteLine("Program does not exist!");
+                        break;
+                }
+
+            }
+        }
+    }
+}
