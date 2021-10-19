44b0b50da5c9c2cc9853f639d10f41ddfad11572
unknown <stanlin.caceres@gmail.com>
2021-10-19 17:40:19 -0400

    String: Exercise 3 completed

---
+++ String/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using String_Exercises;
+
+namespace string_exercises_menu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continueLoop = true;
+            int number = 0;
+
+            while (continueLoop == true)
+            {
+                continueLoop = Exercises.UtilityMethods.getValidProgramNumber(out number);
+
+                switch (number)
+                {
+                    case 1:
+                        Exercise_01.Execute();
+                        break;
+
+                    case 2:
+                        Exercise_02.Execute();
+                        break;
+
+                    default:
+                        Console.WriteLine("Program does not exist!");
+                        break;
+                }
+
+            }
+        }
+    }
+}
---
+++ String/StringExercises.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Exercises;
+
+namespace String_Exercises
+{
+    public static class Exercise_01
+    {
+        /*
+         * 1. Write a program in C# Sharp to input a string and print it. 
+            Test Data :
+            Input the string : Welcome, w3resource
+            Expected Output :
+
+            The string you entered is : Welcome, w3resource 
+        */
+
+        public static void Execute()
+        {
+            string inputString = UtilityMethods.GetValidString();
+            Console.WriteLine("{0}", inputString);
+        }
+    }
+
+    public static class Exercise_02
+    {
+        /*
+         * 2. Write a program in C# Sharp to find the length of a string without using library function. 
+        Test Data :
+        Input the string : w3resource.com
+        Expected Output :
+
+        Length of the string is : 15 
+        */
+
+        public static void Execute()
+        {
+            string theString = UtilityMethods.GetValidString();
+
+            char[] charArray = theString.ToCharArray();
+
+            Console.WriteLine($"Length of the string is: {charArray.Length}");
+
+        }
+    }
+
+    public static class Exercise_03
+    {
+        /*
+          
+         */
+
+        public static void Execute()
+        {
+
+        }
+    }
+}
